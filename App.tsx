df945bf79d4dd540a5b8e049f867b2eb4c37146e
misha-pavlov <m.pavlov@apiko.com>
2023-12-15 14:05:49 -0600

    feat(app): add fonts

--- App.tsx
+++ App.tsx
@@ -1,59 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useCallback, useEffect, useState } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
+import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { GluestackUIProvider, Text, View } from "@gluestack-ui/themed";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        // await Font.loadAsync(Entypo.font);
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
+  const [fontsLoaded] = useFonts({
+    "Exo2-Regular": require("./assets/fonts/Exo2-Regular.ttf"),
+    "Exo2-Bold": require("./assets/fonts/Exo2-Bold.ttf"),
+    "Exo2-ExtraBold": require("./assets/fonts/Exo2-ExtraBold.ttf"),
+  });
 
   const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
+    if (fontsLoaded) {
       await SplashScreen.hideAsync();
     }
-  }, [appIsReady]);
+  }, [fontsLoaded]);
 
-  if (!appIsReady) {
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text>SplashScreen Demo! 👋</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+      <View onLayout={onLayoutRootView}>
+        <Text fontFamily="Exo2-Regular" style={{ fontSize: 30 }}>
+          SplashScreen Demo! 👋
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
