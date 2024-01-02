c4d0ef74368a8cec76266b82ee383acb3d05015c
misha-pavlov <m.pavlov@apiko.com>
2024-01-01 20:50:28 -0600

    feat(Crypto): add ui

--- types/Crypto.types.ts
+++ types/Crypto.types.ts
@@ -2,5 +2,12 @@ export type Crypto = {
   id: string;
   icon: string;
   name: string;
+  price: string;
   symbol: string;
+};
+
+export type ForecastType = {
+  buyPercentage: number;
+  sellPercentage: number;
+  holdPercentage: number;
 };
